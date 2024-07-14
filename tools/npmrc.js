1783cce118c8d28ffbdcc5e004d3049ff219a867
CakmLexi <calmlexi@hotmail.com>
2024-07-15 00:41:49 +0800

    fix: install error

--- tools/npmrc.js
+++ tools/npmrc.js
@@ -1,4 +1,4 @@
 import fs from 'fs'
 // 创建.npmrc
-fs.writeFileSync('.npmrc', `//npm.pkg.github.com/:_authToken=\${NODE_AUTH_TOKEN}
-@icqqjs:registry=https://npm.pkg.github.com`, 'utf8')
+fs.writeFileSync('.npmrc', `@icqqjs:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=\${NODE_AUTH_TOKEN}`, 'utf8')
