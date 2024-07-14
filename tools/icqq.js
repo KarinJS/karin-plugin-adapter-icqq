4a33c2d62a030c7f97a1ff15e5bffa4974d9e4ca
CakmLexi <calmlexi@hotmail.com>
2024-07-15 01:04:19 +0800

    feat: 添加旧版本

---
+++ tools/icqq.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+const file = './package.json'
+const pkg = JSON.parse(fs.readFileSync(file, 'utf8'))
+
+pkg.dependencies.icqq = 'latest'
+delete pkg.dependencies['@icqqjs/icqq']
+pkg.name = 'karin-plugin-adapter-icqq-old'
+
+pkg.devDependencies = {
+  '@types/express': 'latest',
+  '@types/lodash': 'latest',
+  '@types/node': 'latest',
+  '@types/node-schedule': 'latest',
+  '@types/ws': 'latest',
+  eslint: 'latest',
+  neostandard: 'latest',
+  'node-karin': 'latest',
+  'tsc-alias': 'latest',
+  tsx: 'latest',
+  typescript: 'latest',
+}
+
+fs.writeFileSync(file, JSON.stringify(pkg, null, 2), 'utf8')
+
+// 修改 src/code/index.ts 中的 import @icqqjs/icqq 为 import icqq
+const ts = './src/core/index.ts'
+const code = fs.readFileSync(ts, 'utf8')
+fs.writeFileSync(ts, code.replace('@icqqjs/icqq', 'icqq'), 'utf8')
