66eacace6a3acfdd0310b121b71772a0bc9f07bd
时瑾 <shijinn520@gmail.com>
2024-08-09 15:24:17 +0800

    fix: dirname error

--- src/imports/dir.ts
+++ src/imports/dir.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -6,6 +7,6 @@ const filePath = fileURLToPath(import.meta.url).replace(/\\/g, '/')
 /** 插件包的目录绝对路径 */
 const dirPath = path.resolve(filePath, '../../../')
 /** 插件包的名称 */
-const basename = path.basename(dirPath)
+const basename = JSON.parse(fs.readFileSync(`${dirPath}/package.json`, 'utf-8')).name
 
 export { dirPath, basename }
