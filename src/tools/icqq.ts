40182e62730df4172664385004114b5e2943c837
瑜笙 <2624367622@qq.com>
2025-03-08 14:51:03 +0800

    fix: 更新icqq依赖版本并优化工作流配置

--- src/tools/icqq.ts
+++ src/tools/icqq.ts
@@ -2,17 +2,6 @@ import fs from 'fs'
 const file = './package.json'
 const pkg = JSON.parse(fs.readFileSync(file, 'utf8'))
 
-pkg.dependencies.icqq = 'latest'
-delete pkg.dependencies['@icqqjs/icqq']
-pkg.name = '@karinjs/adapter-icqq-old'
+pkg.dependencies.icqq = '*'
 
 fs.writeFileSync(file, JSON.stringify(pkg, null, 2), 'utf8')
-
-// 修改 src/code/index.ts 中的 import @icqqjs/icqq 为 import icqq
-const ts = './lib/core/index.js'
-const code = fs.readFileSync(ts, 'utf8')
-fs.writeFileSync(ts, code.replace(/@icqqjs\/icqq/gm, 'icqq'), 'utf8')
-
-const ts1 = './lib/index.js'
-const code1 = fs.readFileSync(ts1, 'utf8')
-fs.writeFileSync(ts1, code1.replace(/@icqqjs\/icqq/gm, 'icqq'), 'utf8')
