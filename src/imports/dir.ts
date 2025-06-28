a18bc5b540a59c5c0f942ecf78edaed73babb27c
瑜笙 <2624367622@qq.com>
2025-06-28 16:54:06 +0800

    fix: 更新依赖项并优化插件目录路径解析逻辑

--- src/imports/dir.ts
+++ src/imports/dir.ts
@@ -5,7 +5,11 @@ import { fileURLToPath } from 'url'
 /** 当前文件的绝对路径 */
 const filePath = fileURLToPath(import.meta.url).replace(/\\/g, '/')
 /** 插件包的目录绝对路径 */
-const dirPath = path.resolve(filePath, '../../../')
+let dirPath = path.resolve(filePath, '../../../')
+
+if (!fs.existsSync(path.join(dirPath, 'package.json'))) {
+  dirPath = path.resolve(filePath, '../../')
+}
 /** 插件包的名称 */
 const basename = JSON.parse(fs.readFileSync(`${dirPath}/package.json`, 'utf-8')).name
 
